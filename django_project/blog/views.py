2720327a13f3b99f26f13c99dfe07ebc9d940820
Sam <Sam@DESKTOP-43GT8DL>
2024-04-27 06:58:50 -0600

    Part 4 & 5 - Admin Roles & Database

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,28 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 # Create your views here.
-posts = [
-    {
-        'author': 'Sam Farrell',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'August 27, 2018',
-
-    },
-    {
-        'author': 'Gregor Watson',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'April 24, 2024',
-
-    }
-]
 
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 def about(request):
