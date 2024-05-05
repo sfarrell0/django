1979f338bc5f3f5c6b23c30f734adca3b391152d
CubiksRube1595 <vectorvfd@gmail.com>
2024-05-05 11:59:48 -0600

    user profiles

--- django_project/django_project/urls.py
+++ django_project/django_project/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from users import views as user_views
 urlpatterns = [
     path('profile/', user_views.profile, name='profile'),
@@ -26,3 +28,8 @@ urlpatterns = [
     path('logout/', user_views.logout_view, name='logout'),
     path('', include('blog.urls')),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+else:
+    pass
